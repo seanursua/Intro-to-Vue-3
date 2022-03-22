67f64b4ebbaa083d185a2a133fcf5c98df85258d
seanursua <seandoge@gmail.com>
2022-03-23 00:53:00 +0800

    L2

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-const product = 'Socks'
+const app = Vue.createApp ({
+    data() {
+        return {
+            product: 'Shoes',
+            description: 'My favorite brand of shoe is New Balance'
+        }
+    }
+})
